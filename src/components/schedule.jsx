cfb88e1cc1d4c6ce5c34ed2c29faec2b8813ea8d
Zikri <70962575+zikrikn@users.noreply.github.com>
2024-09-24 14:31:56 +0700

    Update schedule.jsx

--- src/components/schedule.jsx
+++ src/components/schedule.jsx
@@ -22,7 +22,7 @@ const Schedule = () => {
                 `}
             </style>
             <div className="iframe-container">
-                <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FJakarta&title=Jadwal%20Semester%206&mode=WEEK&src=YzM2MGRzeDA4NzZAYmFuZ2tpdC5hY2FkZW15&src=Y183MGYwYWQ4NTIyMGFiMzhmZTJjZWE0OWQ1YmYyMTFiZjI2MGE3ZDJhYzMwOTg1ZjRiYWViOWY4OTFlZGViOWE5QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4uaW5kb25lc2lhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23039BE5&color=%23EF6C00&color=%230B8043"></iframe>
+               <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FJakarta&bgcolor=%23ffffff&title=Zikri's%20Schedule&mode=WEEK&showNav=0&showTabs=0&showCalendars=0&src=emlrcmlraG9saWZhaDg5OEBnbWFpbC5jb20&color=%2333ae06"></iframe>
             </div>
         </>
     );
